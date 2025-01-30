762227946256eaed38dbb84a19580bd062fb4d6a
AdarshMitr <adarshkumar019@gmail.com>
2025-01-30 22:59:50 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div> <h2>
+      OKBzns- <i>Share Success,Grow Together</i>
+      </h2>
+        
+      </div>
+       
+       
+    </>
+  )
+}
+
+export default App
